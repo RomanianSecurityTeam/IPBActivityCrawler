6c3b0b575ffcc2013ac79905fbb8222c30225eb3
Cosmin Gheorghita <gecko.alpad@gmail.com>
2016-09-06 09:47:04 +0300

    Added the instructions to use it

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # IPBActivityCrawler
+
+Instructions
+---
+
+```
+$ git clone https://github.com/RomanianSecurityTeam/IPBActivityCrawler.git
+$ cd IPBActivityCrawler
+$ composer install
+$ nano .env
+```
+
+Inside the .env file, add the following variables and fill them.
+
+```
+DB_HOST=localhost
+DB_NAME=rst_activity_crawler
+DB_USER=root
+DB_PASS=neverguess
+```
+
+First time installation
+---
+
+The first time you install this, you need to run the `includes/install.php` script to create the database schema.
+
+```
+$ php includes/install.php
+```
+
+You can remove it after the installation is done, although it's not going to do you any harm if you keep it.
+
+Updates
+---
+
+If you haven't made any changes to the tracked files, run:
+
+```
+$ git pull
+$ composer update
+```
+
+If you have made changes, you need to re-do all the steps from the Instructions section below, then re-apply your changes. ALternatively, you can stash your changes, pull then apply the stashde changes and remove any conflicts you might encounter.
+
+```
+$ git stash
+$ git pull
+$ git stash apply
+```
+
+Enjoy!
+
+Authors
+---
+Gecko (http://degecko.com)
--- includes/install.php
+++ includes/install.php
@@ -4,20 +4,22 @@ require_once 'config.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-Capsule::schema()->create('activity', function ($table) {
-    $table->increments('id');
-    $table->enum('type', ['rep', 'post', 'topic', 'join', 'follow_thread', 'follow_user', 'profile_photo_change']);
-    $table->string('path')->nullable();
-    $table->string('title')->nullable();
-    $table->longText('content')->nullable();
-    $table->string('author')->nullable();
-    $table->string('author_path')->nullable();
-    $table->string('author_photo')->nullable();
-    $table->string('target')->nullable();
-    $table->string('target_path')->nullable();
-    $table->string('category')->nullable();
-    $table->string('category_path')->nullable();
-    $table->timestamp('created_at');
-});
+if (! Capsule::schema()->hasTable('activity')) {
+    Capsule::schema()->create('activity', function ($table) {
+        $table->increments('id');
+        $table->enum('type', ['rep', 'post', 'topic', 'join', 'follow_thread', 'follow_user', 'profile_photo_change']);
+        $table->string('path')->nullable();
+        $table->string('title')->nullable();
+        $table->longText('content')->nullable();
+        $table->string('author')->nullable();
+        $table->string('author_path')->nullable();
+        $table->string('author_photo')->nullable();
+        $table->string('target')->nullable();
+        $table->string('target_path')->nullable();
+        $table->string('category')->nullable();
+        $table->string('category_path')->nullable();
+        $table->timestamp('created_at');
+    });
+}
 
 die('Installed!');
